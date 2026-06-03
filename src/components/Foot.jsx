0ce9b31944cce8e6cc283ed1c91607d2f4cc613c
Abi04nash <abinashmishra1401@gmail.com>
2026-06-03 12:31:26 +0530

    Some footer

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -7,9 +7,8 @@ const Foot = () => {
     background: "linear-gradient(to right, #f308a9ff, #00bfff)", // deep purple to sky blue
     WebkitBackgroundClip: "text",
     WebkitTextFillColor: "transparent",
-    fontFamily: "'Poppins', sans-serif",
-    // fontSize: "14px",
-    fontWeight: "600",
+    // fontSize: "0.9rem",
+    fontWeight: "500",
     textAlign:"center",
     // letterSpacing: "0.1px"
   }}>Crafted with <i class="fa-solid fa-heart-pulse"></i> by Avi</h5>
