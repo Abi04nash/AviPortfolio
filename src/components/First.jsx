0c1b7cc654ce859e8e4cbeb899fdded1bb2d6c04
Abi04nash <abinashmishra1401@gmail.com>
2026-07-17 18:22:49 +0530

    Resume changed

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -188,7 +188,7 @@ const First = () => {
 
         <motion.main variants={itemVariants} className='contain'>
           <div className='first3'>
-            <motion.div className='a' whileHover={{ scale: 1.00 }} whileTap={{ scale: 0.95 }} onClick={() => window.location.href = 'https://drive.google.com/file/d/1BLnospFs0K1ToSPTRajmO5M-p3cIr1tR/view?usp=sharing'} style={{color:"white"}}> View Resume</motion.div>
+            <motion.div className='a' whileHover={{ scale: 1.00 }} whileTap={{ scale: 0.95 }} onClick={() => window.location.href = 'https://drive.google.com/file/d/1uS2ga0LaaoPRDo9Ik2X8W_mxbjbu3CqB/view?usp=sharing'} style={{color:"white"}}> View Resume</motion.div>
             <motion.div whileHover={{ scale: 1.00 }} whileTap={{ scale: 0.95 }} className='b' onClick={handleNavigation}> Project Gallery</motion.div>
           </div>
         </motion.main>
